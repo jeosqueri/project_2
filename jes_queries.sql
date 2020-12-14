6fdb21efec0ef3f6efe577d00b472a372ea96fb4
jeosqueri <juliasqueri303@gmail.com>
2020-12-13 18:28:41 -0700

    added comments to queries

--- jes_queries.sql
+++ jes_queries.sql
@@ -11,15 +11,15 @@ FROM us_videos;
 SELECT AVG(views) AS avg_views
 FROM us_videos;
 
+-- Most liked video US
 SELECT MAX(likes) AS max_likes
 FROM us_videos;
 
--- most liked video US
 SELECT channel_title, title, likes, dislikes, comment_count
 FROM us_videos
 WHERE likes = (SELECT MAX(likes) FROM us_videos);
 
--- most disliked video US
+-- Most disliked video US
 SELECT MAX(dislikes) AS max_dislikes
 FROM us_videos;
 
@@ -27,7 +27,7 @@ SELECT channel_title, title, dislikes, likes, comment_count
 FROM us_videos
 WHERE dislikes = (SELECT MAX(dislikes) FROM us_videos);
 
--- most commented video US
+-- Most commented video US
 SELECT MAX(comment_count) AS max_comment_count
 FROM us_videos;
 
@@ -36,10 +36,6 @@ FROM us_videos
 WHERE comment_count = (SELECT MAX(comment_count) FROM us_videos);
 
 -- Russia Stats
-
-SELECT MAX(likes) AS max_likes
-FROM ru_videos;
-
 SELECT AVG(likes) AS avg_likes
 FROM ru_videos;
 
@@ -52,24 +48,26 @@ FROM ru_videos;
 SELECT AVG(views) AS avg_views
 FROM ru_videos;
 
--- most liked video RU
+-- Most liked video RU
+SELECT MAX(likes) AS max_likes
+FROM ru_videos;
+
 SELECT channel_title, en_channel_title, title, en_title, likes, dislikes, comment_count
 FROM ru_videos
 WHERE likes = (SELECT MAX(likes) FROM ru_videos);
 
---most disliked video RU
+-- Most disliked video RU
 SELECT channel_title, en_channel_title, title, en_title, likes, dislikes, comment_count
 FROM ru_videos
 WHERE dislikes = (SELECT MAX(dislikes) FROM ru_videos);
 
---most commented video RU
+-- Most commented video RU
 SELECT channel_title, en_channel_title, title, en_title, likes, dislikes, comment_count
 FROM ru_videos
 WHERE comment_count = (SELECT MAX(comment_count) FROM ru_videos);
 
--- Categories
-
---most popular channel & category (measure by number of times they had a video trending)
+-- Categorie Queries
+-- Most popular channel & category (measured by number of times they had a video trending)
 SELECT channel_title, en_channel_title, category_name, COUNT(*) AS channel_count
 FROM ru_join
 GROUP BY channel_title, en_channel_title, category_name
@@ -80,7 +78,7 @@ FROM us_join
 GROUP BY channel_title, category_name
 ORDER BY channel_count DESC;
 
--- most viewed video and channel name per category (for 5 top categories), US
+-- Most viewed video and channel name per category (for 5 top categories), US
 SELECT DISTINCT channel_title, category_name, title, views
 FROM us_join
 WHERE category_name = 'Entertainment'
@@ -106,7 +104,7 @@ FROM us_join
 WHERE category_name = 'People & Blogs'
 ORDER BY views DESC;
 
--- most viewed video and channel name per category (for 5 top categories), RU
+-- Most viewed video and channel name per category (for 5 top categories), RU
 SELECT DISTINCT channel_title, en_channel_title, title, en_title, category_name, views
 FROM ru_join
 WHERE category_name = 'Entertainment'
